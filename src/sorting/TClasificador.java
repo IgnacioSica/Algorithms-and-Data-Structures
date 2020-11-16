1e8732058a96f965ea9c0c902e34c8dad3abc56d
P4ssenger <mignacio.sica@gmail.com>
2020-11-16 15:22:32 -0300

    Update TClasificador.java

--- src/sorting/TClasificador.java
+++ src/sorting/TClasificador.java
@@ -7,18 +7,31 @@ public class TClasificador {
         vector[pos2] = vector[pos1];
         vector[pos1] = temp;
     }
-    
+
     int[] selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int jMin = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if(arr[j] < arr[jMin])
+                if (arr[j] < arr[jMin]) {
                     jMin = j;
+                }
             }
-            if(jMin != i)
+            if (jMin != i) {
                 swap(arr, jMin, i);
+            }
         }
         return arr;
     }
-    
+
+    int[] insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            while ((j >= 0) && (arr[j] > arr[j + 1])) {
+                swap(arr, j, j + 1);
+                j--;
+            }
+        }
+        return arr;
+    }
+
 }
