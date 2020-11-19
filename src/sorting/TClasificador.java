b111afa3788aa89bdf5fde2cda76d5bb5a371545
P4ssenger <mignacio.sica@gmail.com>
2020-11-19 02:23:12 -0300

    bucket sort

--- src/sorting/TClasificador.java
+++ src/sorting/TClasificador.java
@@ -1,5 +1,8 @@
 package sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TClasificador {
 
     private void swap(int[] arr, int p1, int p2) {
@@ -12,11 +15,13 @@ public class TClasificador {
         for (int i = 0; i < arr.length - 1; i++) {
             int jMin = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[jMin]) 
+                if (arr[j] < arr[jMin]) {
                     jMin = j;
+                }
             }
-            if (jMin != i) 
+            if (jMin != i) {
                 swap(arr, jMin, i);
+            }
         }
         return arr;
     }
@@ -46,8 +51,8 @@ public class TClasificador {
         }
         return arr;
     }
-    
-    int[] shellSort(int[] arr){
+
+    int[] shellSort(int[] arr) {
         int j;
         int[] gaps = new int[]{3223, 358, 51, 10, 3, 1};
         for (int gap : gaps) {
@@ -55,8 +60,9 @@ public class TClasificador {
                 for (int i = gap; i < arr.length; i++) {
                     j = i - gap;
                     while (j >= 0) {
-                        if (arr[j] > arr[j + gap]) 
+                        if (arr[j] > arr[j + gap]) {
                             swap(arr, j, j + gap);
+                        }
                         j -= gap;
                     }
                 }
@@ -64,28 +70,71 @@ public class TClasificador {
         }
         return arr;
     }
+
+    int[] countingSort(int[] arr) {
+        int k = 0;
+        int n = arr.length;
+        
+        for (int a : arr) 
+            k = k > a ? k : a;
+
+        int[] count = new int[k + 1];
+
+        for (int x : arr) 
+            ++count[x];
+
+        for (int i = 1; i <= k; ++i) 
+            count[i] += count[i - 1];
+        
+        int[] output = new int[n];
+        
+        for (int i = n - 1; i >= 0; i--) { 
+            output[count[arr[i]] - 1] = arr[i]; 
+            --count[arr[i]]; 
+        }
+        
+        for (int i = 0; i < n; ++i) 
+            arr[i] = output[i];
+
+        return arr;
+    }
+
+    int[] bucketSort(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer>[] bucket = new ArrayList[n];
+        
+        for (int i = 0; i < n; i++)
+            bucket[i] = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) 
+            bucket[arr[i]].add(arr[i]);
+        
+        for (int i = 0; i < n; i++) 
+            Collections.sort((bucket[i]));
     
-    int[] quickSort(int[] arr){
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0, size = bucket[i].size(); j < size; j++) {
+            arr[index++] = bucket[i].get(j);
+            }
+        }
+        return arr;
+    }
+
+    int[] radixSort(int[] arr) {
         return arr;
     }
     
-    int[] countingSort(int[] arr){
+    int[] heapSort(int[] arr) {
         return arr;
     }
     
-    int[] heapSort(int[] arr){
+    
+    int[] quickSort(int[] arr) {
         return arr;
     }
-    
-    int[] bucketSort(int[] arr){
-        return arr;
-    }
-    
-    int[] radixSort(int[] arr){
-        return arr;
-    }
-    
-    int[] mergeSort(int[] arr){
+
+    int[] mergeSort(int[] arr) {
         return arr;
     }
 }
--- src/sorting/main.java
+++ src/sorting/main.java
@@ -4,12 +4,14 @@ public class main {
     public static void main(String[] args){
         TClasificador clasif = new TClasificador();
         GeneradorDatosGenericos gdg = new GeneradorDatosGenericos();
-        int[] arr = gdg.generarDatosAleatorios(20);
+        int[] arr = gdg.generarDatosAleatorios(150);
         for(int i = 0; i < arr.length; i++)
             System.out.print(arr[i]+" ");
-        clasif.bubbleSort(arr);
+        clasif.bucketSort(arr);
         System.out.println("");
         for(int i = 0; i < arr.length; i++)
             System.out.print(arr[i]+" ");
+        
+        int[] count = new int[10];   
     }
 }
